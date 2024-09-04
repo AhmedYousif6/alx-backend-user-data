9f7366b7857e41292ab31be13885186fe1cbbf10
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-09-04 20:46:54 +0100

    task done

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,19 @@
+""" Session Auth system module
+"""
+from .auth import Auth
+import uuid
+
+
+class SessionAuth(Auth):
+    """ session auth class inherits from base template Auth
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ method that create session id for user id
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
