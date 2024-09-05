ddb2811e8be6d409a3de2e68f65736a7cc9dcd11
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-09-05 22:16:56 +0100

    task done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,8 @@
 """
 from .auth import Auth
 import uuid
-
+from flask import request
+from models.user import User
 
 class SessionAuth(Auth):
     """ session auth class inherits from base template Auth
@@ -25,3 +26,9 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """return User instance based on cookie value
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
